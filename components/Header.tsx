728508516becc47076e9e02b2912ac9bfbe80871
RavinduUni <fc115680@foc.sjp.ac.lk>
2025-12-31 20:52:46 +0530

    create-navigation

---
+++ components/Header.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const Header = () => {
+
+  const pathName = usePathname();
+
+  return ( 
+    <header> 
+        <div className='main-container inner'>
+            <Link href="/">
+                <Image src="logo (1).svg" alt='coinpulse logo' width={132} height={40} />
+            </Link>
+
+            <nav>
+                <Link href='/' className={cn('nav-link', {
+                    'is-active': pathName === '/',
+                    'is-home': true
+                })}>Home</Link>
+
+                <p>Search Modal</p>
+
+                <Link href='/coins' className={cn('nav-link', {
+                    'is-active': pathName === '/coins',
+                })}>All Coins</Link>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
