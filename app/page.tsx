c5359faf57a1f9001ce66d433a6724294f854ab8
RavinduUni <fc115680@foc.sjp.ac.lk>
2026-01-03 00:57:35 +0530

    implement coin overview and trending coins ui functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import DataTable from '@/components/DataTable'
+import CoinOverview from '@/components/home/CoinOverview';
+import TrendingCoin from '@/components/home/TrendingCoin';
+import { fetcher } from '@/lib/coingecko.action';
+import { cn, formatCurrency } from '@/lib/utils';
+import { TrendingDown, TrendingUp } from 'lucide-react';
+import Image from 'next/image'
+import Link from 'next/link';
+import React, { Suspense } from 'react'
 
-const page = () => {
+
+
+const page = async () => {
+
   return (
-    <div></div>
+    <main className='main-container'>
+      <section className='home-grid'>
+        <Suspense fallback={ <div>Loading Overview...</div> }>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={ <div>Loading Trending...</div> }>
+          <TrendingCoin />
+        </Suspense>
+      </section>
+
+      <section className='w-full mt-7 space-y-4'>
+        <p>Categories</p>
+      </section>
+    </main>
   )
 }
 
