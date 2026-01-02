14d65bb5373cdf324a5c180d3ad19a9405936986
DERANIYAGALA R.S. <fc115680@foc.sjp.ac.lk>
2026-01-03 01:04:53 +0530

    Update components/DataTable.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -26,7 +26,7 @@ const DataTable = <T,> ({ columns, data, rowKey, tableClassName, headerRowClassN
                 {data.map((row, rowIndex) => (
                     <TableRow key={rowKey(row, rowIndex)} className={cn('overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/30! relative', bodyRowClassName)}>
                         {columns.map((column, columnIndex) => (
-                            <TableCell key={columnIndex} className={cn('py-4 first:pl-5 last:pr-5')}>
+                            <TableCell key={columnIndex} className={cn('py-4 first:pl-5 last:pr-5', bodyCellClassName)}>
                                 {column.cell(row, rowIndex)}
                             </TableCell>
                         ))}
